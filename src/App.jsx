e4adac0d80b00c4d5127193b5dd3255181c96148
crisilto <cristinasilvestre1708@gmail.com>
2024-08-28 20:26:29 +0200

    style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className='app-container'>
       <Header />
       <Controls onReset={resetGrid} onRandomize={randomizeGrid} />
       <ContributionGrid grid={grid} handleClick={handleClick} getColor={getColor} />
